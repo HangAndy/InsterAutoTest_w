c75021bfa36135e159b26b654bce1a70f7f5b17f
xuyonghang <xuyonghang@zed.com>
2020-04-08 10:51:02 +0800

    Initial commit
    Signed-off-by: xuyonghang <xuyonghang@zed.com>

---
+++ config/__init__.py
@@ -0,0 +1,10 @@
+"""
+   ==============================================
+    objectName： InsterAutoTest_w
+    fileName：   __init__.py
+    Author:      Hang
+    Date:        2020/4/8
+    description: 
+   ==============================================
+
+"""
